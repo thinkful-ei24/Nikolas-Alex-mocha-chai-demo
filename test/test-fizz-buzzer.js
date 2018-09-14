dfbebb253259d8141ba642634594b9c821bd09ed
Alex <0alexgutierrez@gmail.com>
2018-09-14 09:26:59 -0700

    finished drills

--- test/test-fizz-buzzer.js
+++ test/test-fizz-buzzer.js
@@ -7,7 +7,7 @@ describe('fizzbuzzer', () => {
   it('should throw error if input is not a number', () => {
     const badInputs = ['dog', 'cat', 'horse'];
     badInputs.forEach(input => {
-      expect(()=> {
+      expect(() => {
         fizzbuzzer(input);
       }).to.throw(Error);
     });
@@ -18,6 +18,29 @@ describe('fizzbuzzer', () => {
     inputs.forEach(input => {
       const answer = fizzbuzzer(input);
       expect(answer).to.equal('fizz-buzz');
+    });
+  });
+
+  it('should be "buzz" if divisible by 5', () => {
+    const inputs = [5, 10, 20];
+    inputs.forEach(input => {
+      const answer = fizzbuzzer(input);
+      expect(answer).to.equal('buzz');
+    });
+  });
+
+  it('should be "fizz" if divisible by 3', () => {
+    const inputs = [3, 6, 9];
+    inputs.forEach(input => {
+      const answer = fizzbuzzer(input);
+      expect(answer).to.equal('fizz');
+    });
+  });
+  it('should return itself(num) if not divisible by 15, 5 or 3', () => {
+    const inputs = [2, 4, 7];
+    inputs.forEach(input => {
+      const answer = fizzbuzzer(input);
+      expect(answer).to.equal(input);
     });
   });
 });
